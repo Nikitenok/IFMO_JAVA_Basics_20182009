9eb233f04e3ce28872baecd2f5a126c706f99ffa
Mr.Cool <kylin98@list.ru>
2018-11-04 18:02:15 +0300

    commet

--- src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
+++ src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
@@ -1,5 +1,8 @@
 package ru.ifmo.cet.javabasics;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Нужно реализовать констурктор и метод, возвращающий слова песни про бутылки на стене.
  * <p>
@@ -30,14 +33,93 @@ package ru.ifmo.cet.javabasics;
  * Значение передается в качестве параметра конструктора
  * Нужно ограничить возможность взятия бутылок натуральным число не более 99 бутылок за раз.
  */
-public class BottleSong {
+public class BottleSong  {
+
+    Map<Integer, String> hashMap = new HashMap<Integer, String>();
+    int bottles = 99;
+    String song = "";
 
     public BottleSong(int bottleTakenAtOnce) {
-        //TODO
+
+        if(bottleTakenAtOnce > bottles || bottleTakenAtOnce <= 0) {
+            throw new IllegalArgumentException("wrong number of bottles");
+        }
+
+        FullMap();
+
+        WriteSong(bottleTakenAtOnce, getTakenBottles(bottleTakenAtOnce));
+
+    }
+
+    private String getTakenBottles(int num){
+        if(num >= 10) {
+            if (num < 20) return hashMap.get(num);
+            else if (num % 10 == 0)
+                return hashMap.get(num);
+            else
+                return (hashMap.get(num - num % 10) + " " + hashMap.get(num % 10));
+        }else return hashMap.get(num);
+
+    }
+
+    private void WriteSong(int taken, String taken_word){
+
+
+        while (bottles > 0){
+
+            if(bottles > taken)
+            {
+                song += Integer.toString(bottles) + " " + (bottles == 1 ? "bottle" : "bottles") + " of beer on the wall, " +
+                            Integer.toString(bottles) + " " + (bottles == 1 ? "bottle" : "bottles") + " of beer.\n" +
+                            "Take " + taken_word + " down and pass around, " +
+                            Integer.toString(bottles - taken) + " " + (bottles - taken == 1 ? "bottle" : "bottles") + " of beer on the wall.\n";
+            }
+            else
+                {
+                song += Integer.toString(bottles) + " " + (bottles == 1 ? "bottle" : "bottles") + " of beer on the wall, " +
+                        Integer.toString(bottles) + " " + (bottles == 1 ? "bottle" : "bottles") + " of beer.\n" +
+                        "Take " + getTakenBottles(bottles) +
+                        " down and pass around, no more bottles of beer on the wall.\n";
+                }
+                bottles -= taken;
+        }
+        song += "No more bottles of beer on the wall, no more bottles of beer.\n" +
+                "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
+
+    }
+
+    private void FullMap(){
+        hashMap.put(0,"");
+        hashMap.put(1,"one");
+        hashMap.put(2,"two");
+        hashMap.put(3,"three");
+        hashMap.put(4,"four");
+        hashMap.put(5,"five");
+        hashMap.put(6,"six");
+        hashMap.put(7,"seven");
+        hashMap.put(8,"eight");
+        hashMap.put(9,"nine");
+        hashMap.put(10,"ten");
+        hashMap.put(11,"eleven");
+        hashMap.put(12,"twelve");
+        hashMap.put(13,"thirteen");
+        hashMap.put(14,"fourteen");
+        hashMap.put(15,"fifteen");
+        hashMap.put(16,"sixteen");
+        hashMap.put(17,"seventeen");
+        hashMap.put(18,"eighteen");
+        hashMap.put(19,"nineteen");
+        hashMap.put(20,"twenty");
+        hashMap.put(30,"thirty");
+        hashMap.put(40,"forty");
+        hashMap.put(50,"fifty");
+        hashMap.put(60,"sixty");
+        hashMap.put(70,"seventy");
+        hashMap.put(80,"eighty");
+        hashMap.put(90,"ninety");
     }
 
     public String getBottleSongLyrics() {
-        //TODO
-        throw new UnsupportedOperationException();
+        return song;
     }
 }
