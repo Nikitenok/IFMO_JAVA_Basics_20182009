b599f0b43c02c7a0c5fa507063c3086913740c8a
Mr.Cool <kylin98@list.ru>
2018-11-13 03:16:43 +0300

    commet

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,21 +1,88 @@
 package ru.ifmo.cet.javabasics;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
+
+import static java.nio.file.Files.readAllLines;
 
 
 public class WarAndPeaceExercise {
 
-    public static String warAndPeace() {
+    public static String warAndPeace() throws IOException {
+        Map<String, Integer> words = new HashMap<>();
+        String s = "";
+
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
 
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
+        for (String line : readAllLines(tome12Path, Charset.forName("windows-1251"))) {
+            line = line.replaceAll("[^а-яА-Яa-zA-Z]", " ");
+            for (String word : line.split(" ")) {
+                if (word.length() >= 4) {
+                    word = word.toLowerCase();
+                    if ((words.containsKey(word))) {
+                        words.put(word, words.get(word) + 1);
+                    } else {
+                        words.put(word, 1);
+                    }
+                }
+            }
+        }
 
-        throw new UnsupportedOperationException();
+        for (String line : readAllLines(tome34Path, Charset.forName("windows-1251"))) {
+            line = line.replaceAll("[^а-яА-Яa-zA-Z]", " ");
+            for (String word : line.split(" ")) {
+                if (word.length() >= 4) {
+                    word = word.toLowerCase();
+                    if (words.containsKey(word)) {
+                        words.put(word, words.get(word) + 1);
+                    } else {
+                        words.put(word, 1);
+                    }
+                }
+            }
+        }
+
+        List<Map.Entry<String,Integer>> sort = new ArrayList<>(words.entrySet());
+
+
+
+        Collections.sort(sort, new Comparator<Map.Entry<String,Integer>>() {
+
+            @Override
+
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+
+                if(o1.getValue().equals(o2.getValue()))
+
+                    return o1.getKey().compareTo(o2.getKey());
+
+                else
+
+                    return o2.getValue().compareTo(o1.getValue());
+
+            }
+
+        });
+
+
+        for(Map.Entry entry: sort){
+
+            String key = (String) entry.getKey();
+
+            Integer value = (Integer) entry.getValue();
+
+            if (value >= 10) {
+
+                s += key + " - " + value + "\n";
+
+            }
+
+        }
+
+        return s.trim();
     }
-
 }
