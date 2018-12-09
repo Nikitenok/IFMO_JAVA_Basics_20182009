932ff4d09183d7d199ba166cc3a73936dc18c82c
Mr.Cool <kylin98@list.ru>
2018-12-10 00:55:37 +0300

    commet3

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,21 +1,34 @@
 package ru.ifmo.cet.javabasics;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.nio.file.Files.readAllLines;
 
 
 public class WarAndPeaceExercise {
 
-    public static String warAndPeace() {
+    public static String warAndPeace() throws IOException {
+
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
 
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO Iff word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order iff needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
+        String txt = readAllLines(tome12Path, Charset.forName("windows-1251")).stream().collect(Collectors.joining(" "));
+        txt += readAllLines(tome34Path, Charset.forName("windows-1251")).stream().collect(Collectors.joining(" "));
+        txt = txt.replaceAll("[^a-zA-Zа-яА-Я]", " ").toLowerCase();
 
-        throw new UnsupportedOperationException();
+        Map<String, Integer> dict = new HashMap<>();
+        Stream<String> textStr = Stream.of(txt.split(" "));
+        textStr.forEach((String key) -> dict.put(key, dict.containsKey(key) ? dict.get(key) + 1 : 1));
+
+        List<Map.Entry<String, Integer>> sortedDictionary = new ArrayList<>(dict.entrySet());
+        Collections.sort(sortedDictionary, (o1, o2) -> (o1.getValue().equals(o2.getValue())) ? o1.getKey().compareTo(o2.getKey()) : o2.getValue().compareTo(o1.getValue()));
+        return sortedDictionary.stream().filter(m -> (m.getKey().length() >= 4) && (m.getValue() >= 10))
+                .map(m -> m.getKey() + " - " + m.getValue()).collect(Collectors.joining("\n"));
     }
-
 }
